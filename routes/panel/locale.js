fcb8b74bfb9b0d6f95562081ecdf0d15d7f4c703
mrvet <alexoovsyanikov@gmail.com>
2018-08-31 11:39:15 +0300

    Localization work

--- routes/panel/locale.js
+++ routes/panel/locale.js
@@ -11,6 +11,6 @@ router.get('/locale/langs-list' , LocaleController.GetLanguagesListAction );
 router.post('/locale/new-lang' , LocaleController.AddNewLanguage );
 router.get('/locale/const-list/:lng', LocaleController.GetConstList);
 router.post('/locale/new',LocaleController.AddNewConstLeng);
-
+router.get('/locale/new-lang' , LocaleController.AddNewLanguageAction );
 
 module.exports = router;
