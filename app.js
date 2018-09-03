71808acd5a1115b5361717d6004544deeb77437d
mrvet <alexoovsyanikov@gmail.com>
2018-09-03 09:47:30 +0300

    Promo-codes work

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const indexRouter = require('./routes/index');
 const productsRoutes = require('./routes/panel/products');
 const categoriesRoutes = require('./routes/panel/categories');
 const localeRoutes = require('./routes/panel/locale');
+const productsApiRoutes = require('./routes/api/products');
 
 const fileUpload = require('express-fileupload');
 
@@ -30,10 +31,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ADMIN PANEL ROUTES
 app.use('/', indexRouter);
 app.use('/panel' , productsRoutes);
 app.use('/panel' , categoriesRoutes);
 app.use('/panel' , localeRoutes);
+
+//API ROUTES
+app.use('/api' , productsApiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
