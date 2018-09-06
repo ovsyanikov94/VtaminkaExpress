106c305862a06a1fc46beaaf11e074cd6367bdbb
mrvet <alexoovsyanikov@gmail.com>
2018-09-06 09:05:58 +0300

    Promo-codes work

--- controller/panel/LocaleController.js
+++ controller/panel/LocaleController.js
@@ -5,6 +5,7 @@ const WordsConstans =require('../../model/defenitions').WordsConstans;
 const Translations =require('../../model/defenitions').Translations;
 const Response = require('../../model/Response');
 const fs = require('fs');
+const connection = require('../../routes/connection');
 
 module.exports.UpdateConst = async(req,res)=>{
 
@@ -49,7 +50,7 @@ module.exports.UpdateConst = async(req,res)=>{
     res.status( respone.code );
     res.send(respone);
 
-}
+};
 
 module.exports.RemoveConst = async(req,res)=>{
     let respone = new Response();
@@ -60,7 +61,7 @@ module.exports.RemoveConst = async(req,res)=>{
 
             respone.code=404;
             respone.message="значение нн найдено";
-            respone.data = idConst
+            respone.data = idConst;
             return res.send(respone)
 
         }
@@ -80,7 +81,7 @@ module.exports.RemoveConst = async(req,res)=>{
 
         respone.code=200;
         respone.message = "константа удалена";
-        respone.data = idConst
+        respone.data = idConst;
         res.send(respone)
     }
     catch (ex){
@@ -102,7 +103,7 @@ module.exports.AddNewConstLeng=async(req,res)=>{
             'constantTitle':title,
             'description':description,
 
-        })
+        });
 
 
 
@@ -140,7 +141,6 @@ module.exports.GetConstList = async (req,res)=>{
     }
 
 };
-
 
 module.exports.GetLanguagesListAction = async ( req , res )=>{
 
@@ -232,62 +232,114 @@ module.exports.AddNewLanguage = async ( req , res )=>{
 
 };
 
+module.exports.UpdateLanguageAction  = async(req, res)=>{
 
-module.exports.UpdateLang = async ( req , res )=>{
+
+
+    try{
+        let langID = +req.params.id;
+        let lang = await Langs.findById( langID );
+
+        res.render('locale/language-single',{ lang: lang });
+    }//try
+    catch (ex){
+        res.render('error',{ error: ex});
+    }//catch
+};
+
+module.exports.UpdateLanguage = async ( req , res )=>{
 
     let response = new Response();
 
     try{
 
-        let langID = +req.params.id;
-        let languageTitle = req.body.languageTitle.trim();
+        let langID = req.params.id;
 
-        if( isNaN(langID) ){
+        let lang = await Langs.findById(langID);
 
-            response.code = 400;
-            response.message = 'ID языка задан не верно!';
+        if(!lang ){
+
+            response.code = 404;
+            response.message = 'Язык не найден!';
             response.data = langID;
+            res.status(response.code);
 
             return res.send(response);
 
         }//if
 
-        let lang = await Langs.findById(langID);
+        let langTitle = req.body.langTitle;
 
-        if( lang ){
+        //Начало работы с загруженным файлом
+        if( req.files ){
 
-            let updateResult = await Langs.update({
-                'languageTitle': languageTitle
+            let langImage = req.files.image;
+            let path = `public/images/langs/${langID}`;
+
+
+            if(!lang.languageImage){
+
+                try{
+                    if(!fs.existsSync('public/images')){
+                        fs.mkdirSync('public/images');
+                    }
+
+                    if(!fs.existsSync('public/images/langs')){
+                        fs.mkdirSync('public/images/langs');
+                    }
+
+
+                    fs.mkdirSync(path);
+                }//try
+                catch(ex){
+                    console.log(ex);
+                }//catch
+
+
+            }//if
+            else{
+                try{
+                    fs.unlinkSync(`public/${lang.languageImage}`);
+                }
+                catch(ex){
+                    console.log(ex);
+                }
+
+
+            }//else
+
+
+            // fs.existsSync()
+            langImage.mv( `${path}/${langImage.name}` ,async function(err) {
+
+                if (err){
+                    console.log('FILE UPLOAD ERROR:' , err);
+                    return;
+                }//if
+
+                await lang.update({
+                    languageTitle: langTitle,
+                    languageImage: `images/langs/${langID}/${langImage.name}`,
+                });
+
             });
 
-            response.code = 200;
-            response.message = 'Язык успешно обновлен';
-            response.data = updateResult;
-
-            res.send(response);
-
         }//if
-        else{
 
-
-            response.code = 404;
-            response.message = 'Язык не найден!';
-            response.data = langID;
-
-            res.send(response);
-
-        }//else
+        response.code = 200;
+        response.message = 'Язык успешно обновлен!';
 
     }//try
     catch(ex){
 
         response.code = 500;
-        response.message = 'Внутренняя ошибка сервера';
-        response.data = ex;
-
-        res.send( response );
+        response.message = 'Внутренняя ошибка сервера!';
+        console.log(ex);
 
     }//catch
+
+    res.status(response.code);
+    res.send(response);
 
 };
 
@@ -327,7 +379,7 @@ module.exports.RemoveLang= async ( req , res )=>{
         response.code = 200;
         response.message = 'Язык успешно обновлен';
 
-        res.send(response);
+
 
     }//try
     catch(ex){
@@ -337,6 +389,195 @@ module.exports.RemoveLang= async ( req , res )=>{
         response.message = 'Внутренняя ошибка сервера';
         response.data = ex;
 
+    }//catch
+
+    res.status( response.code );
+    res.send(response);
+
+};
+
+module.exports.LanguageExist = async ( req , res )=>{
+
+    let response = new Response();
+
+    try{
+
+        let languageTitle = req.query.languageTitle;
+
+        let lang = await Langs.findOne({
+           where:{
+               languageTitle: languageTitle
+           }
+        });
+
+        if( !lang ){
+
+            response.code = 200;
+            response.data = true;
+
+        }//if
+        else{
+            response.code = 400;
+            response.data = false;
+        }//else
+
+    }//try
+    catch(ex){
+
+        response.code = 500;
+        response.message = 'Внутренняя ошибка сервера';
+        response.data = ex;
+
+    }//catch
+
+    res.status( response.code );
+    res.send( response );
+
+};
+
+module.exports.ImportLanguage = async ( req , res )=>{
+
+    let response = new Response();
+
+    try{
+
+        let file = req.files.file;
+
+        if(file){
+
+            let LangID = req.body.langID;
+
+            let lang = await Langs.findById(LangID);
+
+            let fileData = JSON.parse(file.data);
+
+            fileData = Object.entries(fileData);
+
+            let checkTranstate;
+
+            for(let i = 0; i < fileData.length; i++){
+
+                checkTranstate = await connection.query(`
+                    SELECT *
+                    FROM \`translations\` AS t
+                    JOIN \`wordsconstants\` AS wc ON wc.constantID = t.constantID
+                    WHERE t.langID = '${LangID}' AND wc.constantTitle = '${fileData[i][0]}'`);
+
+
+                if(checkTranstate[0].length > 0){
+
+                    if(fileData[i][1] === ""){
+
+                        Translations.destroy({
+                            where: {
+                                ID: checkTranstate[0][0].ID
+                            }
+                        });
+
+                    }//if
+                    else {
+
+                        let translate = await Translations.findById(checkTranstate[0][0].ID);
+
+                        translate.update({
+                            translation: fileData[i][1]
+                        })
+
+                    }//else
+
+                }//if
+                else {
+
+                    if(fileData[i][1] === "")
+                        continue;
+
+                    let constant = await WordsConstans.findOne({
+
+                        where: {
+                            constantTitle: fileData[i][0]
+                        }
+
+                    });
+
+                    Translations.create({
+                        translation: fileData[i][1],
+                        constantID: constant.constantID,
+                        langID: LangID
+                    });
+
+                }//else
+
+            }//for
+
+            let path = `public/i18n/${lang.dataValues.languageTitle}.json`;
+
+            if(fs.existsSync(path)){
+
+                fs.unlinkSync(path);
+
+            }//if
+
+            fs.appendFileSync(path, file.data);
+
+        }//if
+
+        response.code = 200;
+        response.message = 'Данные успешно добавлены!';
+        response.data = {};
+
+    }//try
+    catch(ex){
+
+        response.code = 500;
+        response.message = 'Внутренняя ошибка сервера!';
+        console.log('EX: ' , ex);
+
+    }//catch
+
+    res.status( response.code );
+    res.send( response );
+
+};
+
+module.exports.ExportLanguage = async ( req , res )=>{
+
+    try{
+
+        let lang = await Langs.findById(req.params.id);
+
+        let translations = await connection.query(`
+            SELECT wc.constantTitle, t.translation
+            FROM \`translations\` as t
+            JOIN \`wordsconstants\` as wc ON wc.constantID = t.constantID
+            WHERE t.languageID = '${lang.languageID}'
+        `);
+
+        let constants = await WordsConstans.findAll();
+
+        let jsonData = {};
+
+        constants.forEach(item => {
+
+            jsonData[`${item.constantTitle}`] = "";
+
+        });
+
+        translations[0].forEach(item => {
+
+            jsonData[`${item.constantTitle}`] = item.translation;
+
+        });
+
+        res.send( jsonData );
+
+    }//try
+    catch(ex){
+
+        let response = new Response();
+        response.code = 500;
+        response.message = 'Внутренняя ошибка сервера!';
+        console.log('EX: ' , ex);
+        res.status( response.code );
         res.send( response );
 
     }//catch
